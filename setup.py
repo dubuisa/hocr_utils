9a140a29ff65d0de2d2fda5806ab4aa542890c43
dubuisa <adubuis@groupemutuel.ch>
2021-06-24 10:57:08 +0200

    add test-requirements

--- setup.py
+++ setup.py
@@ -1,15 +1,16 @@
 from setuptools import find_packages
 from setuptools import setup
+import pathlib
 
-with open('requirements.txt') as f:
-    content = f.readlines()
-requirements = [x.strip() for x in content if 'git+' not in x]
-
+HERE = pathlib.Path(__file__).parent
+INSTALL_REQUIRES = (HERE / "requirements.txt").read_text().splitlines()
+TESTS_REQUIRE = (HERE / "test-requirements.txt").read_text().splitlines()[1:]
 setup(name='hocr_utils',
       version="1.0",
       description="Package containing utility function for hOCR and tesseract",
       packages=find_packages(),
-      install_requires=requirements,
+      install_requires=INSTALL_REQUIRES,
+      tests_require=TESTS_REQUIRE,
       test_suite='tests',
       # include_package_data: to install data from MANIFEST.in
       include_package_data=True,
